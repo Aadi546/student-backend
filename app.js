4c01d24aa8eeab56a8324b0aed5aab48ec4cedfc
Aadi <aadiiit2024@gmail.com>
2025-11-28 18:59:52 +0530

    force CORS deploy

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 require('dotenv').config();           // loads MONGODB_URI from your .env
 console.log('MONGODB_URI =', process.env.MONGODB_URI);
-// Temporary CORS setup
-const cors = require('cors');
-app.use(cors({ origin: '*' }));  // Allow all for now (hackathon)
 
+// Import dependencies
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const errorHandler = require('./src/middleware/errorHandler');
 const app = express();
 
+// Middleware
+app.use(cors({ origin: '*' }));
 app.use(express.json());              // enables parsing JSON bodies
 app.use(errorHandler);
+
 const studentRoutes = require('./src/routes/studentRoutes');
 app.use('/students', studentRoutes);
 
